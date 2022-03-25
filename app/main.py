ecd02acd7ccd181bf73ce83ec9e4f411694cf70c
John <John@vlae.ru>
2022-03-25 23:20:29 +0300

    changed main function

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 """Генератор приветствий."""
-import pprint
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
+def greeting(name: str) -> str:
+    """Возвращает текст приветствия.
 
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+    Args:
+        name: Имя пользователя
+
+    Returns:
+        Текст приветствия
+    """
+    name = ' '.join([word.capitalize() for word in name.split()])
+    # Name Full_name
+    return 'Привет, {0}'.format(name)
